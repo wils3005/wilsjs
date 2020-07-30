f56e9b48becc3cf8ee8dd9c432f2d2f5be8bb513
Jack Wilson <jack@swiftmedical.io>
2020-07-30 11:41:45 -0400

    Thu, 30 Jul 2020 15:41:45 GMT
     5 files changed, 90 insertions(+), 24 deletions(-)

--- packages/my-sqlite3/src/index.ts
+++ packages/my-sqlite3/src/index.ts
@@ -1,22 +1,65 @@
-import { Database } from "sqlite3";
+import * as faker from "faker";
+import * as uuid from "uuid";
+import { Database, RunResult, Statement } from "sqlite3";
+
+type Person = Record<string, string>;
 
 const db = new Database(":memory:");
 
-if (db instanceof Database) {
-  db.serialize(function () {
-    db.run("CREATE TABLE lorem (info TEXT)");
-    let stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-
-    for (const i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-    }
-
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-      console.log(row.id + ": " + row.info);
-    });
-  });
-
-  db.close();
+function handleRunResult(this: RunResult, err: Error | null) {
+  console.error({ this: this, err });
 }
+
+function handleStatement(this: Statement, err: Error | null) {
+  console.error({ this: this, err });
+}
+
+function handleError(err: Error) {
+  console.error({ err });
+}
+
+db.serialize(() => {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS people(
+      id TEXT PRIMARY KEY,
+      first_name TEXT,
+      last_name TEXT,
+      country_code TEXT,
+      state TEXT,
+      street_address TEXT,
+      zip TEXT,
+      phone_number TEXT
+    ) WITHOUT ROWID;
+  `;
+
+  db.run(sql, handleRunResult);
+  const statement = db.prepare(
+    "INSERT INTO people VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    handleStatement
+  );
+
+  for (let i = 0; i < 10; i++) {
+    const params = [
+      uuid.v4(),
+      faker.name.firstName(),
+      faker.name.lastName(),
+      "US",
+      faker.address.stateAbbr(),
+      faker.address.streetAddress(),
+      faker.address.zipCode(),
+      faker.phone.phoneNumber(),
+    ];
+
+    statement.run(params, handleRunResult);
+  }
+
+  statement.finalize(handleError);
+
+  db.each("SELECT * FROM people", (err, row: Person) => {
+    err ? console.error({ err }) : console.info(row);
+  });
+});
+
+db.close((err: Error | null) => {
+  console.error({ err });
+});
